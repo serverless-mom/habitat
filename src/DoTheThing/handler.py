a5693ae7b96c46871c7e1d3ec2366ce07092086f
Tabatha Y <tabathadelane@gmail.com>
2020-02-22 11:23:25 -0800

    update DoTheThing to update streakLength

--- src/DoTheThing/handler.py
+++ src/DoTheThing/handler.py
@@ -2,28 +2,40 @@ import os
 import boto3
 dynamodb = boto3.resource('dynamodb')
 def handler(event, context):
-  table_name = os.environ['TABLE_NAME'] # get the table name from the automatically populated environment variables
-  table = dynamodb.Table(table_name)
+    table_name = os.environ['TABLE_NAME'] # get the table name from the automatically populated environment variables
+    table = dynamodb.Table(table_name)
 
-  result = {
-    'id': '8476', # we only have one user for development
-    'lastDone': '02/21/2020, 10:38:00', # a random time from yesterday
-    'streakLength': '32' # random number for development
-  }
-
-  # add one to streakLength
-  # create new params object that has the new streak length and a date time of right now
-  # do this:
-    table.update_item(Item = params) # do not overwrite existing entries
+    user_id = '8476'
+    get_user = table.get_item(Key={'id': user_id})
 
 
-#do that:
-  response = {
-    'statusCode': 200,
-    'body': 'this should be a new streak length'
-  }
+    streak = get_user['streakLength']
+    streak += 1
+    
+    # result = {
+    # 'id': '8476', # we only have one user for development
+    # 'lastDone': '02/21/2020, 10:38:00', # a random time from yesterday
+    # 'streakLength': '32' # random number for development
+    # }
 
-  #done!
-  
-  return response
+    params = {
+        'id': '8476',
+        'lastDone': '02/21/2020, 10:38:00', 
+        'streakLength': streak
+    }
+    
+    # add one to streakLength
+    # create new params object that has the new streak length and a date time of right now
+    # do this:
+    table.update_item(Item = params) 
+
+    #do that:
+    response = {
+        'statusCode': 200,
+        'body': streak
+    }
+
+    #done!
+    
+    return response
 

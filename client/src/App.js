403d079d9d2c5bf7fff08f270c512c360826990c
Marie-Ehrman <marie.e.ehrman@gmail.com>
2020-02-22 13:09:29 -0800

    Add axios library for API calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,66 @@
-import  {React, Component} from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
-
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-import Home from './Componenets/Home';
-import Feedback from './Componenets/Feedback';
-
-
-
-
+import Home from './Components/Home';
+import Feedback from './Components/Feedback';
 
 
 class App extends Component {
 
-  constructor() {
+    constructor() {
 
-      super();
-      this.state = {
-          streakLength: null
-      };
+        super();
+        this.state = {
+            streakLength: null
+        };
 
+    }
+
+
+  //API call to get streak length? Then pass to Feedback
+  componentDidMount() {
+    this.streakLengthQuery();
   }
-//API call to get streak length? Then pass to Feedback
-componentDidMount() {
-  this.streakLengthQuery();
-}
 
-streakLengthQuery = () => {
-  axios.get('PLACEHOLDER URL')
-    .then(response => {
-      this.setState({
-        streakLength: response.streakLength
+
+
+  streakLengthQuery = () => {
+    axios.get('PLACEHOLDER URL')
+      .then(response => {
+        this.setState({
+          streakLength: response.streakLength
+        });
+      })
+      .catch (error => {
+        console.log('Error fetching the streak data', error);
       });
-    })
-    .catch (error => {
-      console.log('Error fetching the streak data', error);
-    });
+  }
+
+
+
+
+      render(){
+
+        return(
+
+            <BrowserRouter>
+              <div className="container">
+                <Switch>
+                  <Route exact path='/' component={Home}></Route>
+                  <Route path='/feedback' render={() => <Feedback streak={this.state.streakLength} />}></Route>
+                </Switch>
+              </div>
+
+               
+            </BrowserRouter>
+
+        );
+      }
 }
-
-
-
-
-
-
-
-  return (
-
-      <BrowserRouter>
-          <Route exact path='/' component={Home}></Route>
-          <Route path='/feedback' render={() => <Feedback streak={this.state.streakLength} />}></Route>
-      </BrowserRouter>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
-}
-
 export default App;
